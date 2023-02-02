1149f0d8de05307cc932e25dd034e7a8a3280332
Abbas <shah.s@ufl.edu>
2023-02-02 14:09:42 -0600

    Added routing and simple title-bar and home components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { HomeComponent } from './home/home.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
+const routes: Routes =
+[
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+]
+
+@NgModule
+({
+  declarations:
+  [
+    AppComponent,
+    TopBarComponent,
+    HomeComponent
   ],
-  imports: [
+  imports:
+  [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
