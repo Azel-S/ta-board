e7f3766605514b282bc2faee7a529f28f11f6ea2
Riley Willis <willis.rileyp@gmail.com>
2023-02-05 23:37:37 -0500

    Added skeleton directories for the TA-Bot backend

---
+++ backend/pkg/routes/ta-routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	//"TA_DATABASE/pkg/controllers"
+
+	"github.com/gorilla/mux"
+)
+
+var RegisterTARoutes = func(router *mux.Router) {
+	//router.HandleFunc("/ta/", controllers.CreateAcct)
+}
