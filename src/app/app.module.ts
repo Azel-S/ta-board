05a692547fa192d33431dd987da0a214fdb92448
Abbas <shah.s@ufl.edu>
2023-02-06 17:27:01 -0600

    Routing now works correctly. Added login page. use "admin" to sign in. Added empty home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router'
-
-import { AppComponent } from './app.component';
-import { DashComponent } from './dash/dash.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+
+// Custom Components
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { HomeComponent } from './home/home.component'; 
+import { DashComponent } from './dash/dash.component';
+import { LoginComponent } from './login/login.component';
+
+// Material Modules
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
-import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
 
 const routes: Routes =
 [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'dash', component: DashComponent },
+  { path: '',   redirectTo: '/login', pathMatch: 'full' }
 ]
 
 @NgModule
@@ -26,6 +39,8 @@ const routes: Routes =
     AppComponent,
     TopBarComponent,
     DashComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports:
   [
@@ -38,7 +53,12 @@ const routes: Routes =
     MatIconModule,
     MatButtonModule,
     MatToolbarModule,
-    LayoutModule
+    LayoutModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTabsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component
+({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent
+{
+  constructor(public router: Router){}
+
+  // Input fields
+  courseID: string | undefined;
+  password: string | undefined;
+  confirmPassword: string | undefined;
+
+  login()
+  {
+    if(this.courseID == "admin")
+    {
+      this.router.navigate(['\dash']);
+    }
+    else
+    {
+      // TODO: Error Message
+    }
+  }
+
+  register()
+  {
+    // TODO
+  }
+}
