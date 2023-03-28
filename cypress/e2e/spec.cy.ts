b129f8329c87882918d1ba399b56a47da0626a8e
Carlo Quick <carlo.quick@ufl.edu>
2023-03-28 19:29:20 -0400

    Syllabus Button + Teacher Location CyTesting

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -24,3 +24,41 @@ describe('Student can Login', () => {
     cy.visit(url + '/student-view')
   })
 })
+
+describe('Student can Open Syllabus', () => {
+  it('passes', () => {
+    cy.visit(url + '/login')
+    cy.contains('Course ID').type('Admin');
+    cy.get("button#SubmitS").should('be.visible').click();
+
+    // TODO: Check why this login is not interacting with db.
+    cy.visit(url + '/student-view')
+    cy.get("button#syllabusButton").should('be.visible').click();
+  })
+})
+
+describe('Check Login Location Exists', () => {
+  it('passes', () => {
+    cy.visit(url + '/login')
+    // Ensure login is successful:
+    // Successfully route to `/profile` path
+    cy.location('pathname').should('eq', '/login')
+  })
+})
+
+describe('Check Teacher Page Location', () => {
+  it('passes', () => {
+    cy.visit(url + '/teacher-view')
+    // Ensure login is successful:
+    // Successfully route to `/profile` path
+    cy.location('pathname').should('eq', '/teacher-view')
+  })
+})
+
+// describe('Check Teacher View Card Number', () => {
+//   it('passes', () => {
+//     cy.visit(url + '/teacher-view')
+//     cy.location('pathname').should('eq', '/teacher-view')
+//     cy.
+//   })
+// })
