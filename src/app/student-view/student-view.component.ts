e6dd6f5db7387793fd8e91aeab1e952e6d98157d
Abbas Shah <s.4bbas@gmail.com>
2023-04-07 16:43:41 -0500

    Added a GetTime() function, and modified GetDate() function to return current time if no values given. Added to Carlo's form addition. Linked functions for teacher-view for the modify/delete button, they still do nothing, but when backend is ready, the process should be easier.

--- src/app/student-view/student-view.component.ts
+++ src/app/student-view/student-view.component.ts
@@ -14,12 +14,23 @@ import { HttpClient } from '@angular/common/http';
 
 export class StudentViewComponent {
   constructor(public serve_comm: DataComponentService, private serve_back: DataBackendService, private http: HttpClient) { }
+  addActive: boolean = true;
+  description: string = "";
 
-  addActive:boolean = true;
-
-  toggleActive(){
+  toggleActive() {
     this.addActive = !this.addActive;
+    this.description = "What is your question...?";
   }
 
-}
+  submit() {
+    // TODO: Submit question.
+    // this.serve_back.AddQuestion(...)
 
+    // TODO: Delete as this is being faked
+    this.serve_comm.GetQuestions().push({ index: 1, date: new Date(), question: this.description, answer: "No response" });
+
+    this.serve_comm.Notify("Question was successfully added to the list!");
+
+    this.toggleActive();
+  }
+}
