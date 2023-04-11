9bae21f62c3bec1de21f74c4b7a699d1fa020879
Carlo Quick <carlo.quick@ufl.edu>
2023-04-10 20:37:10 -0400

    Local Service: Changes to Save and GetData

--- src/app/services/local.service.ts
+++ src/app/services/local.service.ts
@@ -7,21 +7,13 @@ export class LocalService {
 
   constructor() { }
 
-  public saveData(data: any){
-    //var status={ l: loggedIn, s: JSON.stringify(serial), c: JSON.stringify(course) };
-    
-    localStorage.setItem('status', JSON.stringify(data));
-    // var testObject ={name:"test", time:"Date 2017-02-03T08:38:04.449Z"};
-    // localStorage.setItem('testObject', JSON.stringify(testObject));
+  saveData(key: string, data: any){
+    localStorage.setItem(key, JSON.stringify(data));
   }
 
-  public getData(key: string) {
+  getData(key: string) {
+    console.log('recObj: ', JSON.parse(localStorage.getItem(key)!));
     
-    console.log(JSON.parse(localStorage.getItem('status')));
-    
-    //const recObj = localStorage.getItem('status');
-    //const loggedInObj = 
-    // var retrievedObject = localStorage.getItem('testObject');
-    // console.log('retrievedObject: ', JSON.parse(retrievedObject));
+    return JSON.parse(localStorage.getItem(key)!);
   }
 }
