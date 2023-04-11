8b422e00791cef4026adadf43fe2b977b5513685
Carlo Quick <carlo.quick@ufl.edu>
2023-04-10 20:26:44 -0400

    Local Storage: Save and Get Data

--- src/app/services/local.service.ts
+++ src/app/services/local.service.ts
@@ -7,11 +7,21 @@ export class LocalService {
 
   constructor() { }
 
-  public saveData(loggedIn: string, serial: number, course: number){
-    localStorage.setItem(loggedIn, JSON.stringify(serial));
+  public saveData(data: any){
+    //var status={ l: loggedIn, s: JSON.stringify(serial), c: JSON.stringify(course) };
+    
+    localStorage.setItem('status', JSON.stringify(data));
+    // var testObject ={name:"test", time:"Date 2017-02-03T08:38:04.449Z"};
+    // localStorage.setItem('testObject', JSON.stringify(testObject));
   }
 
   public getData(key: string) {
-    return localStorage.getItem(key)
+    
+    console.log(JSON.parse(localStorage.getItem('status')));
+    
+    //const recObj = localStorage.getItem('status');
+    //const loggedInObj = 
+    // var retrievedObject = localStorage.getItem('testObject');
+    // console.log('retrievedObject: ', JSON.parse(retrievedObject));
   }
 }
