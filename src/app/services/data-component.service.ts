fe7571f876f357881ead8c4380800be54b341e6c
Abbas Shah <s.4bbas@gmail.com>
2023-04-13 19:40:02 -0500

    Fixed data-component service after merge.

--- src/app/services/data-component.service.ts
+++ src/app/services/data-component.service.ts
@@ -15,7 +15,7 @@ export class DataComponentService {
   // F - False
   // S - Student
   // T - Teacher
-  //status: { loggedIn: string, serial: number, course: number } = { loggedIn: "F", serial: 0, course: 0 };
+  status: { loggedIn: string, serial: number, course: number } = { loggedIn: "F", serial: 0, course: 0 };
 
   professor: string = "John Doe";
 
@@ -24,12 +24,22 @@ export class DataComponentService {
   questions: { date: Date, question: string, answer: string }[] = [];
 
   //==Serial Functions==// 
-  SetSerial(serial: number) { this.saveData("status.serial", serial); }
-  GetSerial() { return this.getData("status.serial")  || 0; }
+  SetSerial(serial: number) {
+    this.saveData("status.serial", serial);
+  }
+
+  GetSerial() {
+    return this.getData("status.serial") || 0;
+  }
 
   //==Logged-In Functions==//
-  SetLoggedIn(loggedIn: string) { this.saveData("status.loggedIn", loggedIn); }
-  GetLoggedIn() { return this.getData("status.loggedIn") || "F"; }
+  SetLoggedIn(loggedIn: string) {
+    this.saveData("status.loggedIn", loggedIn);
+  }
+
+  GetLoggedIn() {
+    return this.getData("status.loggedIn") || "F";
+  }
 
   SetCurrentCourse(index: number) {
     this.status.course = index;
@@ -78,124 +88,118 @@ export class DataComponentService {
     else {
       return this.courses[index];
     }
-    else
-      return null;
   }
 
-  GetCourseID(index: number = -1) {
-    if (index == -1) {
-      return this.courses[this.status.course].id;
-    }
-    else {
-      return this.courses[index].id;
-    }
-    else
-      return null;
+GetCourseID(index: number = -1) {
+  if (index == -1) {
+    return this.courses[this.status.course].id;
+  }
+  else {
+    return this.courses[index].id;
+  }
+}
+
+GetCourseName(index: number = -1) {
+  if (index == -1) {
+    return this.courses[this.status.course].name;
+  }
+  else {
+    return this.courses[index].name;
+  }
+}
+
+AddCourse(course: {
+  course_serial: number, course_id: string, description: string, course_name: string, id: number, professor_name: string
+}) {
+  this.courses.push({ serial: course.course_serial, id: course.course_id, name: course.course_name, passcode: "", description: course.description });
+}
+
+ClearCourses() {
+  this.courses = [];
+}
+
+GetNumQuestions() {
+  return this.questions.length;
+}
+
+GetQuestions() {
+  return this.questions;
+}
+
+GetQuestion(index: number = 0) {
+  return this.questions[index];
+}
+
+GetAnswer(index: number = 0) {
+  return this.questions[index].answer;
+}
+
+SetAnswer(index: number = 0, answer: string) {
+  this.questions[index].answer = answer;
+  this.snackBar.open("Response Submitted!", "Close", { duration: 3000 });
+}
+
+AddQuestion(question: { index: number, date: Date, question: string, answer: string }) {
+  this.questions.push(question);
+}
+
+ClearQuestions() {
+  this.questions = [];
+}
+
+OpenSyllabus() {
+  // TODO: Implement actual syllabus
+  window.open('https://www.africau.edu/images/default/sample.pdf', '_blank');
+}
+
+GetDate(date: Date = new Date(), time: boolean = false) {
+  let result: string = "";
+
+  result += date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth();
+  result += "/";
+  result += date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+  result += "/";
+  result += date.getFullYear();
+
+  if (time) {
+    result += " " + this.GetTime(date);
   }
 
-  GetCourseName(index: number = -1) {
-    if (index == -1) {
-      return this.courses[this.status.course].name;
-    }
-    else {
-      return this.courses[index].name;
-    }
-    else
-      return null;
+  return result;
+}
+
+GetTime(date: Date = new Date()) {
+  let result = "";
+
+  if (date.getHours() % 12 == 0) {
+    result += "12";
+  } else {
+    result += date.getHours() % 12 < 12 ? "0" + date.getHours() % 12 : date.getHours() % 12;
   }
+  result += ":";
+  result += date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  result += date.getHours() < 12 ? "am" : "pm";
 
-  AddCourse(course: {
-    course_serial: number, course_id: string, description: string, course_name: string, id: number, professor_name: string
-  }) {
-    this.courses.push({ serial: course.course_serial, id: course.course_id, name: course.course_name, passcode: "", description: course.description });
-  }
-
-  ClearCourses() {
-    this.courses = [];
-  }
-
-  GetNumQuestions() {
-    return this.questions.length;
-  }
-
-  GetQuestions() {
-    return this.questions;
-  }
-
-  GetQuestion(index: number = 0) {
-    return this.questions[index];
-  }
-
-  GetAnswer(index: number = 0) {
-    return this.questions[index].answer;
-  }
-
-  SetAnswer(index: number = 0, answer: string) {
-    this.questions[index].answer = answer;
-    this.snackBar.open("Response Submitted!", "Close", { duration: 3000 });
-  }
-
-  AddQuestion(question: { index: number, date: Date, question: string, answer: string }) {
-    this.questions.push(question);
-  }
-
-  ClearQuestions() {
-    this.questions = [];
-  }
-  
-  OpenSyllabus() {
-    // TODO: Implement actual syllabus
-    window.open('https://www.africau.edu/images/default/sample.pdf', '_blank');
-  }
-
-  GetDate(date: Date = new Date(), time: boolean = false) {
-    let result: string = "";
-
-    result += date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth();
-    result += "/";
-    result += date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
-    result += "/";
-    result += date.getFullYear();
-
-    if (time) {
-      result += " " + this.GetTime(date);
-    }
-
-    return result;
-  }
-
-  GetTime(date: Date = new Date()) {
-    let result = "";
-
-    if (date.getHours() % 12 == 0) {
-      result += "12";
-    } else {
-      result += date.getHours() % 12 < 12 ? "0" + date.getHours() % 12 : date.getHours() % 12;
-    }
-    result += ":";
-    result += date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-    result += date.getHours() < 12 ? "am" : "pm";
-
-    return result;
-  }
+  return result;
+}
 
   //==Local Storage==//
   private saveData(key: string, data: any) {
-    console.log(key + '= ', JSON.parse(localStorage.getItem(key)!));
-    localStorage.setItem(key, JSON.stringify(data));
-  }
+  console.log(key + '= ', JSON.parse(localStorage.getItem(key)!));
+  localStorage.setItem(key, JSON.stringify(data));
+}
 
   private getData(key: string) {
-    console.log(key + ': ', JSON.parse(localStorage.getItem(key)!));
-    return JSON.parse(localStorage.getItem(key)!);
-  }
+  console.log(key + ': ', JSON.parse(localStorage.getItem(key)!));
+  return JSON.parse(localStorage.getItem(key)!);
+}
 
   private removeData(key: string) {
-    localStorage.removeItem(key);
-  }
+  localStorage.removeItem(key);
+}
 
   private clearData() {
-    localStorage.clear();
-  }
+  localStorage.clear();
+}
 
 }
