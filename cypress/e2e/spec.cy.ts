6c0ce1087862e9a8c856483599d68e33f2316f8a
Carlo Quick <carlo.quick@ufl.edu>
2023-04-18 20:10:12 -0400

    Student-View: Cypress Testing

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -52,7 +52,8 @@ describe('Homepage -> Login Page', () => {
 describe('Student can Login', () => {
   it('passes', () => {
     cy.visit(url + '/login')
-    cy.contains('Course ID').type('Admin');
+    cy.contains('Course ID').type('CEN3031');
+    cy.contains('Course Code').type('#0000');
     cy.get("button#SubmitS").should('be.visible').click();
 
     // INFO: Seems that cypress doesn't allow access to two origins.
@@ -61,29 +62,125 @@ describe('Student can Login', () => {
   })
 })
 
-describe('Student can Open Syllabus', () => {
+describe('Ask Question Button Exists', () => {
   it('passes', () => {
+    cy.visit(url + '/login')
+    cy.contains('Course ID').type('CEN3031');
+    cy.contains('Course Code').type('#0000');
+    cy.get("button#SubmitS").should('be.visible').click();
+
     // INFO: Seems that cypress doesn't allow access to two origins.
     //       Currently we are forcing a visit, need a better solution.
     cy.visit(url + '/student-view')
-    cy.get("button#syllabusButton").should('be.visible').click();
+    cy.get("button#AskQuestion").should('be.visible').click();
   })
 })
 
-describe('Teacher can Open Syllabus', () => {
+describe('Ask Question Field Exists', () => {
   it('passes', () => {
+    cy.visit(url + '/login')
+    cy.contains('Course ID').type('CEN3031');
+    cy.contains('Course Code').type('#0000');
+    cy.get("button#SubmitS").should('be.visible').click();
+
     // INFO: Seems that cypress doesn't allow access to two origins.
     //       Currently we are forcing a visit, need a better solution.
-    cy.visit(url + '/course-view')
-    cy.get("button#syllabusButton").should('be.visible').click();
+    cy.visit(url + '/student-view')
+    cy.get("button#AskQuestion").should('be.visible').click();
+    cy.contains('What is your question...?');
   })
 })
+
+describe('Student Can submit question', () => {
+  it('passes', () => {
+    cy.visit(url + '/login')
+    cy.contains('Course ID').type('CEN3031');
+    cy.contains('Course Code').type('#0000');
+    cy.get("button#SubmitS").should('be.visible').click();
+
+    // INFO: Seems that cypress doesn't allow access to two origins.
+    //       Currently we are forcing a visit, need a better solution.
+    cy.visit(url + '/student-view')
+    cy.get("button#AskQuestion").should('be.visible').click();
+    cy.contains('What is your question...?').type('When is your birthday?');
+    cy.get("button#SubmitQuestion ").should('be.visible').click();
+  })
+})
+
+describe('Student Can Cancel Question', () => {
+  it('passes', () => {
+    cy.visit(url + '/login')
+    cy.contains('Course ID').type('CEN3031');
+    cy.contains('Course Code').type('#0000');
+    cy.get("button#SubmitS").should('be.visible').click();
+
+    // INFO: Seems that cypress doesn't allow access to two origins.
+    //       Currently we are forcing a visit, need a better solution.
+    cy.visit(url + '/student-view')
+    cy.get("button#AskQuestion").should('be.visible').click();
+    cy.contains('What is your question...?').type('When is your birthday?');
+    cy.get("button#CancelQuestion").should('be.visible').click();
+  })
+})
+
+describe('Verify Submit and Cancel Do Not Exist After Cancelling', () => {
+  it('passes', () => {
+    cy.visit(url + '/login')
+    cy.contains('Course ID').type('CEN3031');
+    cy.contains('Course Code').type('#0000');
+    cy.get("button#SubmitS").should('be.visible').click();
+
+    // INFO: Seems that cypress doesn't allow access to two origins.
+    //       Currently we are forcing a visit, need a better solution.
+    cy.visit(url + '/student-view')
+    cy.get("button#AskQuestion").should('be.visible').click();
+    cy.contains('What is your question...?').type('When is your birthday?');
+    cy.get("button#CancelQuestion").should('be.visible').click();
+    cy.get("button#CancelQuestion").should('not.exist');
+    cy.get("button#SubmitQuestion").should('not.exist');
+  })
+})
+
+describe('Verify Question Posts after Submission', () => {
+  it('passes', () => {
+    cy.visit(url + '/login')
+    cy.contains('Course ID').type('CEN3031');
+    cy.contains('Course Code').type('#0000');
+    cy.get("button#SubmitS").should('be.visible').click();
+
+    // INFO: Seems that cypress doesn't allow access to two origins.
+    //       Currently we are forcing a visit, need a better solution.
+    cy.visit(url + '/student-view')
+    cy.get("button#AskQuestion").should('be.visible').click();
+    cy.contains('What is your question...?').type('When is your birthday?');
+    cy.get("button#SubmitQuestion").should('be.visible').click();
+    cy.contains('When is your birthday?');
+  })
+})
+
+// describe('Student can Open Syllabus', () => {
+//   it('passes', () => {
+//     // INFO: Seems that cypress doesn't allow access to two origins.
+//     //       Currently we are forcing a visit, need a better solution.
+//     cy.visit(url + '/student-view')
+//     cy.get("button#syllabusButton").should('be.visible').click();
+//   })
+// })
+
+// describe('Teacher can Open Syllabus', () => {
+//   it('passes', () => {
+//     // INFO: Seems that cypress doesn't allow access to two origins.
+//     //       Currently we are forcing a visit, need a better solution.
+//     cy.visit(url + '/course-view')
+//     cy.get("button#syllabusButton").should('be.visible').click();
+//   })
+// })
 
 describe('Check Teacher View Card Numbers', () => {
   it('passes', () => {
     // INFO: Seems that cypress doesn't allow access to two origins.
     //       Currently we are forcing a visit, need a better solution.
     cy.visit(url + '/teacher-view')
-    cy.get('mat-card').should('have.length', 5)
+    cy.get('mat-card').should('have.length', 1)
   }) 
 })
